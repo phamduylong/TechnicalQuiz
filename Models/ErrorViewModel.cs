7eb4ec1ccec4be98011da0ee33d405328fcc1ba8
longph <longphamduy2002@gmail.com>
2023-04-02 22:26:39 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechnicalQuiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Quiz.cs
@@ -0,0 +1,18 @@
+﻿public class Quiz
+{
+    public Quiz(string _title, string _answer, List<string> _options) 
+    {
+        Title = _title;
+        Answer = _answer;
+        Options = _options;
+        Id = Guid.NewGuid().ToString();
+    }
+    public string Title { get; set; }
+
+    public string Answer { get; set; }
+
+    public List<string> Options { get; set; }
+
+    public string Id { get; set; }
+
+}
