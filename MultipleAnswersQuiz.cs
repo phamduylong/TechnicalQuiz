6c8c66c7fe9bb1e54c0d88e7ddaa46f2f2c8bfef
longph <longphamduy2002@gmail.com>
2023-04-08 14:42:08 +0300

    split quizzes into SingleAnswerQuiz and MultipleAnswersQuiz classes

---
+++ MultipleAnswersQuiz.cs
@@ -0,0 +1,23 @@
+﻿namespace TechnicalQuiz
+{
+	public class MultipleAnswersQuiz
+	{
+		public MultipleAnswersQuiz(string _title, List<string> _answers, List<string> _options)
+		{
+			Title = _title;
+			Answer = _answers;
+			Options = _options;
+			Id = Guid.NewGuid().ToString();
+		}
+		public string Title { get; set; }
+
+		public List<string> Answer { get; set; }
+
+		public List<string> Options { get; set; }
+
+		public bool AllowMultipleChoices { get; set; }
+
+		public string Id { get; set; }
+
+	}
+}
