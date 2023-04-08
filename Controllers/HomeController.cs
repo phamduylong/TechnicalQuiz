33c075ad4cb23cd95d3148f00a0377176b4a0a40
longph <longphamduy2002@gmail.com>
2023-04-08 14:42:38 +0300

    fix temporary type for index page and controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,13 +11,16 @@ namespace TechnicalQuiz.Controllers
 		public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
+			_logger.LogInformation("Logger is on (●'◡'●)");
 		}
 
 		public IActionResult Index()
 		{
-			var quizzes = new List<Quiz>();
-			quizzes.Add(new Quiz("Who created the SvelteKit framework/compiler?", "Rich Harris", new List<string>() { "Bill Gates", "Linus Torsvald", "Ada Lovelace" }, false));
-			quizzes.Add(new Quiz("In what year was Svelte first released", "2016", new List<string>() { "2013", "2018", "2021" }, false));
+			var quizzes = new List<SingleAnswerQuiz>
+			{
+				new SingleAnswerQuiz("Who created the SvelteKit framework/compiler?", "Rich Harris", new List<string>() { "Bill Gates", "Linus Torsvald", "Ada Lovelace" }),
+				new SingleAnswerQuiz("In what year was Svelte first released", "2016", new List<string>() { "2013", "2018", "2021" })
+			};
 			return View(model: quizzes);
 		}
 
