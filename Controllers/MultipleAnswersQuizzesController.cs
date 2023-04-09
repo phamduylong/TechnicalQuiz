2af57658858ec6ec5a3a9f25b05a7a64d1fa7e3c
longph <longphamduy2002@gmail.com>
2023-04-09 09:42:33 +0300

    add controllers for single answer and multiple answers quiz classes

---
+++ Controllers/MultipleAnswersQuizzesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TechnicalQuiz.Models;
+
+namespace TechnicalQuiz.Controllers
+{
+	public class MultipleAnswersQuizzesController : Controller
+	{
+		public IActionResult Index()
+		{
+			var quizzes = new List<MultipleAnswersQuiz>
+			{
+				new MultipleAnswersQuiz("Choose the correct software design patterns:", new List<string> {"Factory", "Builder", "Singleton"}, new List<string>() { "Burger", "Cook", "Vent" }),
+				new MultipleAnswersQuiz("Which of these are real Linux distributions:", new List<string> {"Ubuntu", "Fedora", "Red Hat"}, new List<string>() { "Tux", "Linuxes", "WSL" })
+			};
+			return View(model: quizzes);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Controllers/SingleAnswerQuizzesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TechnicalQuiz.Models;
+
+namespace TechnicalQuiz.Controllers
+{
+    public class SingleAnswerQuizzesController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public SingleAnswerQuizzesController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+			_logger.LogInformation("Logger is on SingleAnswerQuizzesController");
+		}
+
+		public IActionResult Index()
+		{
+			var quizzes = new List<SingleAnswerQuiz>
+			{
+				new SingleAnswerQuiz("Who created the SvelteKit framework/compiler?", "Rich Harris", new List<string>() { "Bill Gates", "Linus Torsvald", "Ada Lovelace" }),
+				new SingleAnswerQuiz("In what year was Svelte first released", "2016", new List<string>() { "2013", "2018", "2021" })
+			};
+			return View(model: quizzes);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
