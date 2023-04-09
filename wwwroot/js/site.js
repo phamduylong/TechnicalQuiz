07e4726bb7ce899bf54881eed6e89cf2184a9f4d
longph <longphamduy2002@gmail.com>
2023-04-09 09:44:39 +0300

    add haveSameContents function to shared js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿function haveSameContents(a, b) {
+    const allContents = new Set([...a, ...b]);
+    for (const v of allContents)
+        if (a.filter(e => e === v).length !== b.filter(e => e === v).length)
+            return false;
+    return true;
+};
