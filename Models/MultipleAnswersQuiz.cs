755eadf1cde3e3a5cffea15d7522729be12d2c91
longph <longphamduy2002@gmail.com>
2023-04-09 09:42:48 +0300

    move classes into models

---
+++ Models/MultipleAnswersQuiz.cs
@@ -0,0 +1,21 @@
+﻿namespace TechnicalQuiz.Models
+{
+    public class MultipleAnswersQuiz
+    {
+        public MultipleAnswersQuiz(string title, List<string> answers, List<string> options)
+        {
+            Title = title;
+            Answers = answers;
+            Options = options;
+            Id = Guid.NewGuid().ToString();
+        }
+        public string Title { get; set; }
+
+        public List<string> Answers { get; set; }
+
+        public List<string> Options { get; set; }
+
+        public string Id { get; set; }
+
+    }
+}
---
+++ Models/SingleAnswerQuiz.cs
@@ -0,0 +1,21 @@
+﻿namespace TechnicalQuiz.Models
+{
+    public class SingleAnswerQuiz
+    {
+        public SingleAnswerQuiz(string title, string answer, List<string> options)
+        {
+            Title = title;
+            Answer = answer;
+            Options = options;
+            Id = Guid.NewGuid().ToString();
+        }
+        public string Title { get; set; }
+
+        public string Answer { get; set; }
+
+        public List<string> Options { get; set; }
+
+        public string Id { get; set; }
+
+    }
+}
