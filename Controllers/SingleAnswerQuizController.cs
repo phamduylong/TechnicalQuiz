f5fc90e5dc7dfe20d8865d26246294388b76e325
longph <longphamduy2002@gmail.com>
2023-04-15 23:31:48 +0300

    add some more quizzes

--- Controllers/SingleAnswerQuizController.cs
+++ Controllers/SingleAnswerQuizController.cs
@@ -19,7 +19,11 @@ namespace TechnicalQuiz.Controllers
 			var quizzes = new List<SingleAnswerQuiz>
 			{
 				new SingleAnswerQuiz("Who created the SvelteKit framework/compiler?", "Rich Harris", new List<string>() { "Bill Gates", "Linus Torsvald", "Ada Lovelace" }),
-				new SingleAnswerQuiz("In what year was Svelte first released", "2016", new List<string>() { "2013", "2018", "2021" })
+				new SingleAnswerQuiz("In what year was Svelte first released", "2016", new List<string>() { "2013", "2018", "2021" }),
+				new SingleAnswerQuiz("Linux 1.0.0 was released in March 1994. How many lines of code were there in the release?", "176,250", new List<string>() { "51,923", "100,097", "252,766" }),
+				new SingleAnswerQuiz("Which version of JavaScript introduced the arrow syntax?", "ECMAScript 2015 (ES6)", new List<string>() { "ECMAScript 5 2009 (ES5)", "ECMAScript 2017", "ECMAScript 2019" }),
+				new SingleAnswerQuiz("How many design patterns was included in the book Design Patterns written by Gangs of Four?", "23", new List<string>() { "15", "29", "31" }),
+
 			};
 			return View(model: quizzes);
 		}
